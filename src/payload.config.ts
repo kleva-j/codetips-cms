21dd46fe3933caf0389d3e3998ec93378b40e212
kleva-j <kasmickleva@gmail.com>
2023-08-17 19:43:19 +0100

    feat: update payload collection and upload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,8 @@
 import { Categories } from "./collections/Categories";
 import { Articles } from "./collections/Articles";
+import { Projects } from "./collections/Projects";
 import { buildConfig } from "payload/config";
+import { Media } from "./collections/Media";
 import { Users } from "./collections/Users";
 
 import path from "path";
@@ -10,11 +12,16 @@ export default buildConfig({
     process.env.NODE_ENV === "production"
       ? process.env.RENDER_EXTERNAL_URL
       : process.env.HOSTNAME,
-  cors: [process.env.FRONTEND_URL], // "*" wildflag
+  cors: "*", // "*" wildflag
   admin: {
     user: Users.slug,
   },
-  collections: [Users, Categories, Articles],
+  collections: [Users, Categories, Articles, Projects, Media],
+  upload: {
+    limits: {
+      fileSize: 5000000, // 5MB, written in bytes
+    },
+  },
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
